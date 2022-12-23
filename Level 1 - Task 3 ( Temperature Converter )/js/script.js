1b2e1c4d3531f4ca4582b895845e243a3b6cbfb9
Ganesh Utla <ganeshutla888@gmail.com>
2022-12-23 16:46:15 +0530

    Major Changes

--- Level 1 - Task 3 ( Temperature Converter )/js/script.js
+++ Level 1 - Task 3 ( Temperature Converter )/js/script.js
@@ -1,56 +1,19 @@
 
-function tempConverter() {
+let celsius = document.getElementById('celsius-ip');
+let kelvin = document.getElementById('kelvin-ip');
+let farh = document.getElementById('farh-ip');
 
-    let temp = parseFloat(document.getElementById('temp-ip').value);
-    let ipUnit = document.getElementById('unit-ip').value;
-    let opUnit = document.getElementById('unit-op').value;
-    var res = 0;
-    var unit = null;
-    
-    if(ipUnit==="celsius"){
-        if(opUnit==="kelvin") {
-            res = temp + 273.15;
-            unit = "°K"
-        }
-        else if(opUnit==="fahrenheit") {
-            res = temp*(9/5) + 32;
-            unit = "°F"
-        } 
-        else {
-            res = temp;
-            unit = "°C"
-        }
-    }
-    
-    else if(ipUnit==="kelvin"){
-        if(opUnit==="celsius") {
-            res = temp - 273.15;
-            unit = "°C"
-        }
-        else if(opUnit==="fahrenheit") {
-            res = (temp - 273.15)*(9/5) + 32;
-            unit = "°F"
-        } 
-        else {
-            res = temp;
-            unit = "°K"
-        }
-    }
-    
-    else{
-        if(opUnit==="celsius") {
-            res = (temp-32)*(5/9);
-            unit = "°C"
-        }
-        else if(opUnit==="kelvin") {
-            res = (temp-32)*(5/9) + 273.15;
-            unit = "°K"
-        } 
-        else {
-            res = temp;
-            unit = "°F"
-        }
-    }
+celsius.oninput = () => {
+    kelvin.value = (parseFloat(celsius.value) + 273.15).toFixed(2);
+    farh.value = (parseFloat(celsius.value)*(9/5) + 32).toFixed(2);
+}
 
-    document.getElementById('result').innerHTML = `Result: ${res.toFixed(2)} ${unit}`;
+kelvin.oninput = () => {
+    celsius.value = (parseFloat(kelvin.value) - 273.15).toFixed(2);
+    farh.value = ((parseFloat(kelvin.value) - 273.15)*(9/5) + 32).toFixed(2);
+}
+
+farh.oninput = () => {
+    celsius.value = ((parseFloat(farh.value)-32)*(5/9)).toFixed(2);
+    kelvin.value = ((parseFloat(farh.value)-32)*(5/9) + 273.15).toFixed(2);
 }
